d7e16088548e2bf42e7625f50cb35a5e67e74379
Kameshwaran <kamesh@codebrahma.com>
2018-08-01 10:27:28 +0530

    Removed styled components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import theme from 'serverless-design-system/src/theme';
 
 import { Flex, Box } from 'serverless-design-system/src';

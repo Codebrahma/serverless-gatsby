b711c596a646f4faf8b16a9ed499afecd05e1c35
Kameshwaran <kamesh@codebrahma.com>
2018-08-02 17:19:39 +0530

    Refactored header with container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Box, Logo } from 'serverless-design-system/src'
+import { Box, Container, Flex, Logo } from 'serverless-design-system/src'
 import Navbar from './Navbar'
 import NavButton from './NavButton';
 
@@ -24,28 +24,30 @@ class Header extends React.Component {
         maxHeight='100%'
         oy={['scroll', 'scroll', 'visible']}
       >
-        <Box
-          px='15px'
-          m='auto'
-          maxWidth='1'
-          display='flex'
-          flexDirection='row'
-          flexWrap='wrap'
-          alignItems='center'
-          justifyContent='space-between'
-        >
-          <Logo
-            src='https://serverless.com/_/src/assets/images/logo.ce91bef30d19103f7a9be878a9b44465.png'
-            height={['22px', '22px', '22px', '30px']}
-            width={['125px', '125px', '125px', '170px']}
-            alt="Serverless"
-          />
-          <NavButton
-            onClick={this.toggleNavbarVisibility}
-            active={isNavbarActive}
-          />
-          <Navbar visibility={isNavbarActive} />
-        </Box>
+        <Container>
+          <Flex
+            maxWidth={1}
+            /* NEED TO REFACTOR FLEX COMPONENT */
+            display='flex'
+            flexDirection='row'
+            flexWrap='wrap'
+            alignItems='center'
+            justifyContent='space-between'
+            px={15}
+          >
+            <Logo
+              src='https://serverless.com/_/src/assets/images/logo.ce91bef30d19103f7a9be878a9b44465.png'
+              height={['22px', '22px', '22px', '30px']}
+              width={['125px', '125px', '125px', '170px']}
+              alt="Serverless"
+            />
+            <NavButton
+              onClick={this.toggleNavbarVisibility}
+              active={isNavbarActive}
+            />
+            <Navbar visibility={isNavbarActive} />
+          </Flex>
+        </Container>
       </Box>
     );
   }

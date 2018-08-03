1c01badfc85367f085917b8dbf798027d3c0a94e
prasanna1211 <prasanna@codebrahma.com>
2018-08-03 11:42:30 +0530

    added changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,6 +14,7 @@ import {
 } from 'serverless-design-system/src';
 
 // Renders Each item in the footer list
+// If used >= 3 move to molecules. (Currently 2)
 const FooterListItem = ({ content, color, pt }) => (
   <Box>
     <P

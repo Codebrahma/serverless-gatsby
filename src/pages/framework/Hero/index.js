5191044c90521d8ea8129cce1ef12938c93c7bfb
prasanna1211 <prasanna@codebrahma.com>
2018-08-04 20:32:06 +0530

    added header and image

--- src/pages/framework/Hero/index.js
+++ src/pages/framework/Hero/index.js
@@ -1,75 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
 import {
-  Box, Button, Container, Flex, Heading, Image, SecondaryButton, ImageCard, Testimonial, P
+  Flex,
+  Box,
+  Container,
 } from 'serverless-design-system/src';
 
-import serverlessBolt from '../../../assets/images/serverlessBolt.png';
-import frameworkIllustrator from '../../../assets/images/frameworkIllustration.png'
+import Header from './Header';
+import HeroImage from './HeroImage';
 
 export default () => (
   <Box width={1}>
     <Box bg='black'>
-      <Container px={2} py={4} mb="400px" maxWidth="800px">
-        <Flex
-          height='86px'
-          display='flex'
-          justifyContent='center'
-          my='16px'
-        >
-          <Image
-            src={serverlessBolt}
-            maxHeight='67px'
-            maxWidth='70px'
-          />
+      <Container
+        px={2}
+        mb="400px"
+        maxWidth={1}
+        width={1}
+      >
+        <Flex flexDirection="column" justifyContent="center">
+          <Header />
+          <HeroImage />
         </Flex>
-        <Heading.h2
-          align='center'
-          fontFamily='Serverless'
-          color='white'
-          lineHeight={0}
-          letterSpacing="-1.1px"
-          fontSize={5}
-          mb={0}
-        >
-          serverless
-        </Heading.h2>
-        <Heading.h1 align='center' fontFamily='Serverless' color='white'>
-          framework
-        </Heading.h1>
-        <P
-          fontSize={3}
-          lineHeight={3}
-          color="white"
-          align="center"
-        >
-          The easy, open way to build serverless applications
-        </P>
-        <P
-          fontSize={3}
-          lineHeight={3}
-          align="center"
-          maxWidth="800px"
-        >
-        The Serverless Framework is an open-source CLI for building and deploying serverless applications. With over 6 million deployments handled, the Serverless Framework is the tool developers trust to build cloud applications.
-        </P>
-        <Flex justifyContent='center'>
-          <Image
-            src={frameworkIllustrator}
-            maxHeight="190px"
-          />
-        </Flex>
-        <Flex
-          flexDirection='row'
-          width={[1, 1, 3/4]}
-          justifyContent='center'
-          mx='auto'
-          my={4}
-        >
-          <Button mx={1} minWidth={'280px'} border={0}>quick start</Button>
-          <SecondaryButton mx={1} minWidth={'280px'}>login</SecondaryButton>
-        </Flex>
-    </Container>    
+      </Container>    
+    </Box>
   </Box>
-</Box>
 );

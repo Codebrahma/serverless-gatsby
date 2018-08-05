1f5f4a96931edecc8243d7963b6e6c0f7e4d80e0
Kameshwaran <kamesh@codebrahma.com>
2018-08-05 10:46:00 +0530

    Fixed testimonial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,16 @@
 import React from 'react'
 
-import { Box, Testimonial } from 'serverless-design-system/src';
-import cocaColaLogo from '../../assets/images/cocacola-logo.png';
+import { Box } from 'serverless-design-system/src';
 import Hero from './Hero';
 import Benefits from './Benefits';
+import Testimonial from './Testimonial';
 import TrustedClients from '../../components/TrustedClients';
 
 export default () => (
   <Box width={1}>
     <Hero />
     <Benefits />
-
-    <Testimonial
-      img={cocaColaLogo}
-      name="Patrick Brandt"
-      designation="Solutions Architect at The Coca Cola Company"
-      alt="Coca Cola"
-    >
-      "The Serverless Framework is a core component of The Coca-Cola Company's initiative to reduce IT operational costs and deploy services faster."
-    </Testimonial>
-
+    <Testimonial />
     <TrustedClients />
   </Box>
 );

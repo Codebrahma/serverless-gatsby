1e7ebe71bbe07bae3120ecadaab486fef2667a58
Kameshwaran <kamesh@codebrahma.com>
2018-08-05 19:25:23 +0530

    Added trusted clients

--- src/components/TrustedClients.js
+++ src/components/TrustedClients.js
@@ -1,69 +1,65 @@
 import React from 'react';
 
-import { Flex, Box, P, Image } from 'serverless-design-system/src';
+import { Flex, Box, Container, P, Image } from 'serverless-design-system/src';
+import backgroundImage from '../assets/images/rectangle-19.png';
+import eaLogo from '../assets/images/ea-logo.png';
+import cocacolaLogo from '../assets/images/cocacola-logo.png';
+import nordstromLogo from '../assets/images/nordstrom-logo.png';
+import expediaLogo from '../assets/images/expedia-logo.png';
+import reutersLogo from '../assets/images/reuters-logo.png';
 
 const TrustedClient = () => (
-  <Flex width="100%" mb={'200px'}>
-    <Box px="15px" width='100%'>
+  <Box
+    width={1}
+    backgroundImage={`url(${backgroundImage})`}
+    py={5}
+    px={[ 0, 0, 1, 4, 8 ]}
+  >
+    <Container>
       <Flex
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
+        alignItems='center'
+        justifyContent='space-between'
+        flexDirection={['column', 'column', 'row']}
+        width={1}
       >
-        <Box>
-          <P fontSize="24px">
-            Serverless platform is trusted by
-          </P>
-        </Box>
-        <Flex
-          display="flex"
-          flexDirection={['column', 'column%', 'row', 'row', 'row']}
-          justifyContent="space-between"
-          alignItems="center"
-          width="100%"
-          px="10%"
-          py="2%"
-          boxSizing="border-box"
-        >
-          <Box mb="25px">
-            <Image
-              src="https://serverless.com/_/src/assets/images/ea-logo.5a55f9447c3626d1f68588cb1ed7d1a6.png"
-              width={['80%', '80%', '80%', '100%', '100%']}
-              maxHeight={['41px', '41px']}
-            />
-          </Box>
-          <Box mb="25px">
-            <Image
-              src="https://serverless.com/_/src/assets/images/cocacola-logo.503c39b412c589b0f1485a2c07091385.png"
-              width={['80%', '80%', '80%', '100%', '100%']}
-              maxHeight={['41px', '41px']}
-            />
-          </Box>
-          <Box mb="25px">
-            <Image
-              src="https://serverless.com/_/src/assets/images/nordstrom-logo.f4f30520b4330d1de1ab3dab430a9caf.png"
-              width={['100%', '80%', '80%', '80%', '80%']}
-              maxHeight={['41px', '41px']}
-            />
-          </Box>
-          <Box mb="25px">
-            <Image
-              src="https://serverless.com/_/src/assets/images/expedia-logo.056101cd8f42ba560c27ddfa4ae925ff.png"
-              width={['100%', '80%', '80%', '80%', '80%']}
-              maxHeight={['41px', '41px']}
-            />
-          </Box>
-          <Box mb="25px">
-            <Image
-              src="https://serverless.com/_/src/assets/images/reuters-logo.f1e4ccb495fa3788d3af8269d9f77541.png"
-              width={['100%', '80%', '80%', '80%', '80%']}
-              maxHeight={['41px', '41px']}
-            />
-          </Box>
+        <Image
+          src={eaLogo}
+          alt='EA'
+          maxHeight={['41px', '41px', '41px', '60px']}
+          maxWidth={[ '80%', '80%', '60%', '80%', '100%' ]}
+          my={2}
+        />
+        <Image
+          src={cocacolaLogo}
+          alt='Coca Cola'
+          maxHeight={['41px', '41px', '41px', '60px']}
+          maxWidth={[ '80%', '80%', '60%', '80%', '100%' ]}
+          my={2}
+        />
+        <Image
+          src={nordstromLogo}
+          alt='Nordstrom'
+          maxHeight={['41px', '41px', '41px', '60px']}
+          maxWidth={[ '80%', '80%', '60%', '80%', '100%' ]}
+          my={2}
+        />
+        <Image
+          src={expediaLogo}
+          alt='Expedia'
+          maxHeight={['41px', '41px', '41px', '60px']}
+          maxWidth={[ '80%', '80%', '60%', '80%', '100%' ]}
+          my={2}
+        />
+        <Image
+          src={reutersLogo}
+          alt='Reuters'
+          maxHeight={['41px', '41px', '41px', '60px']}
+          maxWidth={[ '80%', '80%', '60%', '80%', '100%' ]}
+          my={2}
+        />
       </Flex>
-    </Flex>
-    </Box>
-  </Flex>
+    </Container>
+  </Box>
 );
 
 export default TrustedClient;

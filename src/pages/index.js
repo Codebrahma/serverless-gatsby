f4368b5c130c8a8b00d3d1d97efbe673e3164862
Kameshwaran <kamesh@codebrahma.com>
2018-08-06 02:02:34 +0530

    Updated home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
-import HomePage from './Home';
+import React from 'react'
 
-export default HomePage;
+import { Box } from 'serverless-design-system/src';
+import Hero from './Home/Hero';
+import Benefits from './Home/Benefits';
+import Testimonial from './Home/Testimonial';
+import PlatformFeatures from './Home/PlatformFeatures';
+import TrustedClients from '../components/TrustedClients';
+
+export default () => (
+  <Box width={1}>
+    <Hero />
+    <Benefits />
+    <Testimonial />
+    <PlatformFeatures />
+    <TrustedClients />
+  </Box>
+);

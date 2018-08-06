dc9f76078753713a3c46e5dec7c6cbbec5b3945f
prasanna1211 <prasanna@codebrahma.com>
2018-08-06 10:13:45 +0530

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,29 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-root-import',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/assets/images/favicon.png",
+        appName: 'Serverless',
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/',
+        version: '1.0',
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+        }
+      }
+    }
   ],
 }

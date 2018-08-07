c6d0ba6f0e4cb733464524993cffc9d2f7ea0fba
Kameshwaran <kamesh@codebrahma.com>
2018-08-08 00:37:45 +0530

    Updated padding values

--- src/components/Header/Navbar/Menu/Title.js
+++ src/components/Header/Navbar/Menu/Title.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Box, Text } from 'serverless-design-system/src';
 import NavbarContext from '../../NavbarContext';
 
-const MenuTitle = ({ name }) => (
+export default ({ name }) => (
   <NavbarContext.Consumer>
     {
       ({ isNavbarShrinked }) => (
         <Box
-          px={[ '10px', '10px', '8px', '25px' ]}
+          px={[ 1, 1, '8px', '25px' ]}
           py={[
-            '20px',
-            '20px',
+            2,
+            2,
             isNavbarShrinked ? '27.5px' : '35.5px'
           ]}
           transition={[
@@ -32,5 +32,3 @@ const MenuTitle = ({ name }) => (
     }
   </NavbarContext.Consumer>
 );
-
-export default MenuTitle;

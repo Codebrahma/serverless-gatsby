c77fc35980e23e1726dc8188200407149276853f
Kameshwaran <kamesh@codebrahma.com>
2018-08-08 00:20:29 +0530

    Replaced a component

--- src/pages/home/Prefooter.js
+++ src/pages/home/Prefooter.js
@@ -5,12 +5,11 @@ import createPrefooter from '../../components/Prefooter';
 import whiteRectangleDots from '../../assets/images/whiteRectangleDots.png';
 
 const LeftColumn = () => (
-  <Box
+  <Flex.verticallyCenter
     width={[1, 1, 1/2]}
     py={[3, 5, '150px']}
     px={[3, 5, '100px']}
     boxSizing="border-box"
-    justifyContent="center"
     flexDirection="column"
   >
     <TextWithIcon
@@ -25,7 +24,7 @@ const LeftColumn = () => (
     </TextWithIcon>
     <P color="white">Check out all the different use cases for serverless applications. Take a peek at how the Serverless Framework compares to other software.</P>
     <P color="white">Or if you're ready, follow our step-by-step guide to start building a Serverless Application with your favorite language.</P>
-  </Box>
+  </Flex.verticallyCenter>
 );
 
 const RightColumn = () => (

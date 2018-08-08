c89a30f33bd3d3128afed10795cfb5c1c5c56a4d
Kameshwaran <kamesh@codebrahma.com>
2018-08-08 12:22:47 +0530

    Added title wrapper component

--- src/components/Header/Navbar/Menu/Title.js
+++ src/components/Header/Navbar/Menu/Title.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Box, Text } from 'serverless-design-system/src';
 import NavbarContext from '../../NavbarContext';
+
+const TitleWrapperWithLeadingSlash = styled(Box.relative)`
+  &:before {
+    position: absolute;
+    content: ' ';
+    width: 0;
+    height: 0;
+    left: 0;
+    opacity: 0;
+    background: ${(props) => props.theme.colors.serverlessRed}
+    margin-top: -12.5px;
+    top: 50%;
+    transition: all .2s;
+    transition-property: height, width;
+  }
+
+  &:hover {
+    &:before {
+      opacity: 1;
+      left: 5px;
+      height: 25px;
+      width: 5px;
+      transform: rotateZ(200deg);
+    }
+  }
+`;
 
 export default ({ name }) => (
   <NavbarContext.Consumer>
     {
       ({ isNavbarShrinked }) => (
-        <Box
+        <TitleWrapperWithLeadingSlash
           px={[ 1, 1, '8px', '25px' ]}
           py={[ 2, 2, isNavbarShrinked ? '27.5px' : '35.5px' ]}
           transition={[
@@ -23,7 +50,7 @@ export default ({ name }) => (
           >
             {name}
           </Text.span>
-        </Box>
+        </TitleWrapperWithLeadingSlash>
       )
     }
   </NavbarContext.Consumer>

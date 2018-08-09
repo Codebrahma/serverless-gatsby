841ad391a8feb0a6a7ea4346d6d50944a453f155
Kameshwaran <kamesh@codebrahma.com>
2018-08-09 23:55:59 +0530

    Removed the harded coded white color

--- src/components/WhiteButton/index.js
+++ src/components/WhiteButton/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { withTheme } from 'styled-components';
 import { SecondaryButton } from 'serverless-design-system/src';
 
-export default ({ children }) => (
+const comp = ({ children, theme }) => (
   <SecondaryButton
     width="280px"
     borderWidth="2.5px"
     color="white"
     px='2.5rem'
-    theme={{ colors: { primaryColor: '#ffffff', secondaryColor: '#ffffff' } }}
+    theme={{
+      colors: {
+        primaryColor: theme.colors.white,
+        secondaryColor: theme.colors.white,
+      }
+    }}
   >
     { children }
   </SecondaryButton>
 );
+
+export default withTheme(comp);

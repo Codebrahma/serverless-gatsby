19fc304aaa689c7266eec19c16590edeef5eac46
prasanna1211 <prasanna@codebrahma.com>
2018-08-09 10:56:59 +0530

    Added use case dashboard image

--- src/pages/dashboard/UseCase.js
+++ src/pages/dashboard/UseCase.js
@@ -1,31 +1,28 @@
 import React from 'react';
 
 import { UseCases } from 'src/fragments';
-import dashboardIllustration1 from 'src/assets/images/illustration-use-case-1.png';
-import dashboardIllustration2 from 'src/assets/images/illustration-use-case-2.png';
-import dashboardIllustration3 from 'src/assets/images/illustration-use-case-3.png';
 
-const cases = [
+const cases = (useCase1, useCase2, useCase3) => [
   {
     title: 'Onboarding new team members',
     description: 'The Serverless Dashboard makes onboarding new team members easy, quick, and secure. Architecture visualizions help them get up to speed on your applications, sandbox accounts provide them a safe learning enviroment, and user access controls allow for a secure experience.',
-    imgSrc: dashboardIllustration1,
+    imgSrc: useCase1,
   },
   {
     title: 'Operating at scale',
     description: 'The Serverless Dashboard provides the tools you need to build, deploy, and operate Servleress applications at scale. Logging, metrics, and deep observability help you accomplish more with serverless technology.',
-    imgSrc: dashboardIllustration2,
+    imgSrc: useCase2,
   },
   {
     title: 'Operate applications, not infrastructure',
     description: 'The Serverless Dashboard provides an operations experience that is centered on applications, not on infrastrucutre. Regardless of their cloud experience, any developer will have the power to manage and operate serverless applications from development to production.',
-    imgSrc: dashboardIllustration3,
+    imgSrc: useCase3,
   },
 ];
 
-const UseCaseWrapper = () => (
+const UseCaseWrapper = ({ useCase1, useCase2, useCase3 }) => (
   <UseCases
-    cases={cases}
+    cases={cases(useCase1, useCase2, useCase3)}
     useCaseHeader="Serverless Dashboard Use Cases"
     useCaseSubHeader="These are the 3 main use cases for the Serverless Dashboard that you should know about."
   />
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -6,15 +6,39 @@ import UseCase from './UseCase';
 import Features from './Features';
 import Prefooter from './Prefooter';
 
-const Dashboard = () => (
+
+const Dashboard = ({ data }) => (
   <DefaultLayout
     prefooter={Prefooter}
     transparentHeader
   >
     <Hero />
-    <UseCase />
+    <UseCase 
+      useCase1={data.useCase1}
+      useCase2={data.useCase2}
+      useCase3={data.useCase3}
+    />
     <Features />
   </DefaultLayout>
 );
 
 export default Dashboard;
+export const query = graphql`
+query DashboardImgQuery {
+  useCase1: imageSharp(id: { regex: "/use-case-1/" }) {
+    sizes(maxWidth: 360) {
+      ...GatsbyImageSharpSizes
+    }
+  }
+  useCase2: imageSharp(id: { regex: "/use-case-2/" }) {
+    sizes(maxWidth: 360) {
+      ...GatsbyImageSharpSizes
+    }
+  }
+  useCase3: imageSharp(id: { regex: "/use-case-3/" }) {
+    sizes(maxWidth: 360) {
+      ...GatsbyImageSharpSizes
+    }
+  }
+}
+`;

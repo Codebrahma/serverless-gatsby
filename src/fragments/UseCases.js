f9ee7bd7c39b81eb770c4040a7af751eb69f1a5f
Kameshwaran <kamesh@codebrahma.com>
2018-08-09 18:58:52 +0530

    Moved use case component to design system

--- src/fragments/UseCases.js
+++ src/fragments/UseCases.js
@@ -4,49 +4,9 @@ import {
   Box,
   Container,
   Flex,
-  Heading,
-  Image,
   P,
-  ResponsiveStack
+  UseCase,
 } from 'serverless-design-system/src';
-
-const UseCase = ({ title, description, imgSrc, evenIndex }) => {
-  const headerPadding = {
-    [ evenIndex ? 'pl' : 'pr' ] : [ 0, 0, '55px', '110px' ]
-  };
-
-  return (
-    <ResponsiveStack py={[0, 0, 4]}>
-      <Flex.horizontallyCenter
-        flexDirection="column"
-        width={[1, 1, 5/10]}
-        mb={2}
-        order={evenIndex ? [2, 2, 2] : [2, 2, 1]}
-        { ...headerPadding }
-      >
-        <Heading.h3 fontFamily="SoleilBk">
-          {title}
-        </Heading.h3>
-        <P>
-          {description}
-        </P>
-      </Flex.horizontallyCenter>
-      <Flex.horizontallyCenter
-        width={[1, 1, 5/10]}
-        mb={2}
-        order={evenIndex ? [1, 1, 1] : [1, 1, 2]}
-      >
-        <Image
-          src={imgSrc}
-          alt={title}
-          maxWidth={1}
-          width={[ '100%', '80%', '60%', '80%' ]}
-          my="auto"
-        />
-      </Flex.horizontallyCenter>
-    </ResponsiveStack>
-  );
-}
 
 const UseCases = ({ useCaseHeader, useCaseSubHeader, cases }) => {
   return (
@@ -72,10 +32,10 @@ const UseCases = ({ useCaseHeader, useCaseSubHeader, cases }) => {
           cases.map(({ title, description, imgSrc }, index) => (
             <UseCase
               key={title}
-              evenIndex={index % 2 === 1}
               title={title}
               description={description}
               imgSrc={imgSrc}
+              imageFirst={index % 2 === 1}
             />
           ))
         }

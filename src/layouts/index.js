79b6740008fca0bae625d7727d6d3d5fd52bdf2a
Kameshwaran <kamesh@codebrahma.com>
2018-08-09 19:22:34 +0530

    Loaded fonts in global css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,62 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { injectGlobal, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import theme from 'serverless-design-system/src/theme';
 import { Flex } from 'serverless-design-system/src';
-import serverless from '../assets/fonts/serverless.otf';
-import soleil from '../assets/fonts/soleil.otf';
-import soleilBk from '../assets/fonts/soleil-bk.otf';
-import soleilSb from '../assets/fonts/soleil-sb.otf';
-import soleilBkItalic from '../assets/fonts/soleil-bk-italic.otf';
 import './index.css';
-
-injectGlobal`
-  @font-face {
-    font-family: "Serverless";
-    font-weight: normal;
-    font-style: normal;
-    src: local("Serverless"),
-    local("Serverless-Regular"),
-    url('${serverless}') format("opentype");
-  }
-
-  @font-face {
-    font-family: "SoleilBk";
-    font-weight: normal;
-    font-style: normal;
-    src: local("SoleilBk"),
-    local("Soleil-Book"),
-    url('${soleilBk}') format("opentype");
-  }
-
-  @font-face {
-    font-family: "SoleilSb";
-    font-weight: normal;
-    font-style: normal;
-    src: local("SoleilSB"),
-    local("Soleil-Small-Book"),
-    url('${soleilSb}') format("opentype");
-  }
-
-  @font-face {
-    font-family: "Soleil";
-    font-weight: normal;
-    font-style: normal;
-    src: local("Soleil"),
-    local("Soleil"),
-    url('${soleil}') format("opentype");
-  }
-
-  @font-face {
-    font-family: "SoleilBk-Italic";
-    font-weight: normal;
-    font-style: normal;
-    src: local("SoleilBk-Italic"),
-    url('${soleilBkItalic}') format("opentype");
-  }
-`;
 
 const Layout = ({ children, data }) => (
   <Flex width={1}>

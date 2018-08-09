e365c9b56e411c9f9d8e9ca36322dd865a1ccf33
prasanna1211 <prasanna@codebrahma.com>
2018-08-09 22:40:03 +0530

    Added gatsby image in home

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,44 @@ import Benefits from './home/Benefits';
 import PlatformFeatures from './home/PlatformFeatures';
 import Prefooter from './home/Prefooter';
 import TrustedClients from './home/TrustedClients';
+import DefaultLayout from '../layouts/Default';
+import { ImageContext } from '../../components/ImageContextHOC';
 
 const Home = () => {
    return (
-    <DefaultLayout
-      prefooter={Prefooter}
-      transparentHeader
-    >
-      <Hero />
-      <Benefits />
-      <Testimonial />
-      <PlatformFeatures />
-      <TrustedClients />
-    </DefaultLayout>
+     <ImageContext.Provider value={{...data}}>
+      <DefaultLayout
+        prefooter={Prefooter}
+        transparentHeader
+      >
+        <Hero />
+        <Benefits />
+        <Testimonial />
+        <PlatformFeatures />
+        <TrustedClients />
+      </DefaultLayout>
+    </ImageContext.Provider>
   );
  }
 
 export default Home;
+
+export const query = graphql`
+  query HomeImgQuery {
+    frameworkImage: imageSharp(id: { regex: "/illustration-1/" }) {
+      resolutions(height: 86) {
+        ...GatsbyImageSharpResolutions
+      }
+    }
+    dashboardImage: imageSharp(id: { regex: "/illustration-2/" }) {
+      resolutions(width: 70) {
+        ...GatsbyImageSharpResolutions
+      }
+    }
+    gatewayImage: imageSharp(id: { regex: "/illustration-3/" }) {
+      resolutions(width: 70) {
+        ...GatsbyImageSharpResolutions
+      }
+    }
+  }
+`;

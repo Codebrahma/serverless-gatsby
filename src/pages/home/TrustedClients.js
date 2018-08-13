11dd9fc9860293d40ea7cb0e9a279baba7ec7356
prasanna1211 <prasanna@codebrahma.com>
2018-08-13 15:44:43 +0530

    changed trusted client based on SDS

--- src/pages/home/TrustedClients.js
+++ src/pages/home/TrustedClients.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-import { Box, Container } from 'serverless-design-system/src'
-import { TrustedClients } from 'src/fragments'
+import { Box, Container, ImageList } from 'serverless-design-system/src'
+
+import eaLogo from 'src/assets/images/ea-logo.png'
+import cocacolaLogo from 'src/assets/images/cocacola-logo.png'
+import nordstromLogo from 'src/assets/images/nordstrom-logo.png'
+import expediaLogo from 'src/assets/images/expedia-logo.png'
+import reutersLogo from 'src/assets/images/reuters-logo.png'
 
 import backgroundImage from 'src/assets/images/rectangle-19.png'
+
+const TrustedClientList = [
+  { src: eaLogo, altText: 'EA' },
+  { src: cocacolaLogo, altText: 'Coca Cola' },
+  { src: nordstromLogo, altText: 'NordStrom' },
+  { src: expediaLogo, altText: 'Expedia' },
+  { src: reutersLogo, altText: 'Reuters' },
+];
 
 const HomeTrustedClients = () => (
   <Box
@@ -14,7 +27,11 @@ const HomeTrustedClients = () => (
     pb={[300, 300, 12, 12]}
   >
     <Container>
-      <TrustedClients />
+      <ImageList
+        maxHeight={['41px', '41px', '41px', '60px']}
+        maxWidth={['80%', '80%', '60%', '80%', '100%']}
+        list={TrustedClientList}
+      />
     </Container>
   </Box>
 )

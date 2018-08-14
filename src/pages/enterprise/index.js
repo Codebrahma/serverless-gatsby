103fc1eaf88b6eb930b3c49a93b18945fdf410ca
Kameshwaran <kamesh@codebrahma.com>
2018-08-14 14:55:01 +0530

    Updated enterprise page left section

--- src/pages/enterprise/index.js
+++ src/pages/enterprise/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import TrustedClients from 'src/fragments/TrustedClients'
 import DefaultLayout from 'src/layouts/Default'
 
+import Content from './Content';
 import Prefooter from './Prefooter'
 
 const Home = () => {
   return (
-    <DefaultLayout prefooter={Prefooter} transparentHeader>
+    <DefaultLayout prefooter={Prefooter}>
+      <Content />
       <TrustedClients />
     </DefaultLayout>
   )

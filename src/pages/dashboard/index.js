c54d57e31088aca64a571f8b645e69889334dcda
prasanna1211 <prasanna@codebrahma.com>
2018-08-14 11:35:50 +0530

    added context and loaded on index

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -6,12 +6,76 @@ import UseCase from './UseCase'
 import Features from './Features'
 import Prefooter from './Prefooter'
 
-const Dashboard = () => (
-  <DefaultLayout prefooter={Prefooter} transparentHeader>
-    <Hero />
-    <UseCase />
-    <Features />
-  </DefaultLayout>
+import { ImageContext } from '../../components/HOC/ImageContext'
+
+const Dashboard = ({ data }) => (
+  <ImageContext.Provider value={{...data}}>
+    <DefaultLayout prefooter={Prefooter} transparentHeader>
+      <Hero />
+      <UseCase />
+      <Features />
+    </DefaultLayout>
+  </ImageContext.Provider>
 )
 
 export default Dashboard
+
+export const query = graphql`
+query DashboardImgQuery {
+  dashboardIcon: imageSharp(id: { regex: "/icon-platform-dash/" }) {
+    resolutions(width: 86) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+  useCase1: imageSharp(id: { regex: "/use-case-1/" }) {
+    sizes(maxWidth: 360) {
+      ...GatsbyImageSharpSizes_tracedSVG
+    }
+  }
+  useCase2: imageSharp(id: { regex: "/use-case-2/" }) {
+    sizes(maxWidth: 360) {
+      ...GatsbyImageSharpSizes_tracedSVG
+    }
+  }
+  useCase3: imageSharp(id: { regex: "/use-case-3/" }) {
+    sizes(maxWidth: 360) {
+      ...GatsbyImageSharpSizes_tracedSVG
+    }
+  }
+  dashboardIllustration: imageSharp(id: { regex: "/dashboard-illustration/" }) {
+    sizes(maxWidth: 744) {
+      ...GatsbyImageSharpSizes_tracedSVG
+    }
+  }
+  dashboardFeature1: imageSharp(id: { regex: "/dashboard-feature-1/" }) {
+    resolutions(width: 128) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+  dashboardFeature2: imageSharp(id: { regex: "/dashboard-feature-2/" }) {
+    resolutions(width: 128) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+  dashboardFeature3: imageSharp(id: { regex: "/dashboard-feature-3/" }) {
+    resolutions(width: 128) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+  dashboardFeature4: imageSharp(id: { regex: "/dashboard-feature-4/" }) {
+    resolutions(width: 128) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+  dashboardFeature5: imageSharp(id: { regex: "/dashboard-feature-5/" }) {
+    resolutions(width: 128) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+  dashboardFeature6: imageSharp(id: { regex: "/dashboard-feature-6/" }) {
+    resolutions(width: 128) {
+      ...GatsbyImageSharpResolutions_tracedSVG
+    }
+  }
+}
+`;

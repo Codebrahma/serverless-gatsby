311ad3595f3c3b458ca168e5956906675a748e30
prasanna1211 <prasanna@codebrahma.com>
2018-08-16 08:10:46 +0530

    Added container for overview page

--- src/pages/learn/overview/GetStarterd.js
+++ src/pages/learn/overview/GetStarterd.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import { Flex, SecondaryButton, P } from 'serverless-design-system/src'
+import { Flex, SecondaryButton, P, Container } from 'serverless-design-system/src'
 import learnOverviewGetStartedBg from '../../../assets/images/learnOverviewGetStarted.png'  
 const GetStarted = () => (
-  <Flex.column
-    backgroundImage={`url(${learnOverviewGetStartedBg})`}
-    backgroundSize='contain'
-    height='280px'
-    justifyContent='center'
-    alignItem='center'
-  >
-    <P align='center' color='white' fontSize={5} mx={3}>focus on your business logic, not on your servers</P>
-    <SecondaryButton mx='auto' background='white'>get started</SecondaryButton>
-  </Flex.column>
+  <Container maxWidth={1}>
+    <Flex.column
+      backgroundImage={`url(${learnOverviewGetStartedBg})`}
+      backgroundSize='contain'
+      height='280px'
+      justifyContent='center'
+      alignItem='center'
+    >
+      <P align='center' color='white' fontSize={5} mx={3}>focus on your business logic, not on your servers</P>
+      <SecondaryButton mx='auto' background='white'>get started</SecondaryButton>
+    </Flex.column>
+  </Container>
 )
 
 export default GetStarted

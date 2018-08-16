e91cd86fea26ff81bd460300e551cfac4c73f229
prasanna1211 <prasanna@codebrahma.com>
2018-08-16 16:49:26 +0530

    completed resources overview component

--- src/pages/resources/overview/index.js
+++ src/pages/resources/overview/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import DefaultLayout from 'src/layouts/Default'
 import Hero from './Hero'
+import GetInvolved from './GetInvolved'
 
 import Prefooter from './Prefooter'
 
 const ResourcesOverview = () => (
   <DefaultLayout prefooter={Prefooter} transparentHeader>
     <Hero />
+    <GetInvolved />
   </DefaultLayout>
 )
 

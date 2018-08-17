c01fffa9f0399338aae398c6950564432d870e6f
prasanna1211 <prasanna@codebrahma.com>
2018-08-17 12:01:13 +0530

    Added resources pre footer

---
+++ src/pages/resources/meetups/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import DefaultLayout from 'src/layouts/Default'
+
+import Prefooter from 'src/components/pages/resources/meetups/Prefooter'
+
+const ResourcesMeetups = () => (
+  <DefaultLayout prefooter={Prefooter} transparentHeader>
+    Meetup
+  </DefaultLayout>
+)
+
+export default ResourcesMeetups

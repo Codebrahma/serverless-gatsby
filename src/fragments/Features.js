2d68fc0749a9cdcbd9790316090a19440fe83afa
Kameshwaran <kamesh@codebrahma.com>
2018-08-21 17:07:03 +0530

    Removed padding from container to box

--- src/fragments/Features.js
+++ src/fragments/Features.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import {
+  Box,
   Container,
   Feature as FeatureItem,
   Flex,
@@ -8,23 +9,25 @@ import {
 } from 'serverless-design-system/src'
 
 const Features = ({ features }) => (
-  <Container px={2} pb={[300, 300, 10, 10]}>
-    <Flex.column px={[0, 0, 2, 5]} mt={5}>
-      <Heading.h2 align='center' fontFamily='SoleilSb' my={4}>
-        Features
-      </Heading.h2>
-      <Flex.row flexWrap='wrap' mt={3}>
-        {features.map(({ header, img, content }, index) => (
-          <FeatureItem
-            key={header}
-            header={header}
-            content={content}
-            img={img}
-            rightAligned={index % 2 === 0}
-          />
-        ))}
-      </Flex.row>
-    </Flex.column>
+  <Container>
+    <Box px={2} pb={[300, 300, 10, 10]}>
+      <Flex.column px={[0, 0, 2, 5]} mt={5}>
+        <Heading.h2 align='center' fontFamily='SoleilSb' my={4}>
+          Features
+        </Heading.h2>
+        <Flex.row flexWrap='wrap' mt={3}>
+          {features.map(({ header, img, content }, index) => (
+            <FeatureItem
+              key={header}
+              header={header}
+              content={content}
+              img={img}
+              rightAligned={index % 2 === 0}
+            />
+          ))}
+        </Flex.row>
+      </Flex.column>
+    </Box>
   </Container>
 )
 

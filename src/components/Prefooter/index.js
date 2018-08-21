9e3684f70c4e5747b2152d59b5a4859e7255587a
Kameshwaran <kamesh@codebrahma.com>
2018-08-21 17:03:46 +0530

    Added overflow for Prefooter

--- src/components/Prefooter/index.js
+++ src/components/Prefooter/index.js
@@ -5,6 +5,7 @@ import {
   Box,
   Flex,
   Heading,
+  Overflow,
   TextWithIcon,
   Relative,
   ResponsiveStack,
@@ -19,33 +20,38 @@ import whiteRectangleDots from 'src/assets/images/whiteRectangleDots.png'
 const LeftColumn = ({ heading, sentences }) => (
   <Flex.horizontallyCenter
     width={[1, 1, 1 / 2]}
-    py={[3, 5, 8]}
-    px={[2, 4, 6]}
     boxSizing='border-box'
     flexDirection='column'
   >
-    <TextWithIcon
-      iconSrc={whiteRectangleDots}
-      iconHeight='100px'
-      iconWidth='150px'
-      iconTop='-20px'
-      iconLeft='-100px'
-      backgroundSize={[0, 0, 'initial']}
+    <Overflow
+      o="hidden"
+      width={1}
+      py={[3, 5, 8]}
+      px={[2, 4, 6]}
     >
-      <Heading.h2
-        color='white'
-        fontFamily={'SoleilBk'}
-        fontSize={7}
-        letterSpacing={0}
+      <TextWithIcon
+        iconSrc={whiteRectangleDots}
+        iconHeight='100px'
+        iconWidth='150px'
+        iconTop='-20px'
+        iconLeft='-100px'
+        backgroundSize={[0, 0, 'initial']}
       >
-        {heading}
-      </Heading.h2>
-    </TextWithIcon>
-    {sentences.map((sentence, index) => (
-      <P color='white' mt={0} key={index}>
-        {sentence}
-      </P>
-    ))}
+        <Heading.h2
+          color='white'
+          fontFamily={'SoleilBk'}
+          fontSize={7}
+          letterSpacing={0}
+        >
+          {heading}
+        </Heading.h2>
+      </TextWithIcon>
+      {sentences.map((sentence, index) => (
+        <P color='white' mt={0} key={index}>
+          {sentence}
+        </P>
+      ))}
+    </Overflow>
   </Flex.horizontallyCenter>
 )
 

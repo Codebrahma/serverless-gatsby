08db170a172dbaa3a7b0daa79229dc1b3ab8a9e2
Kameshwaran <kamesh@codebrahma.com>
2018-08-23 01:32:23 +0530

    Fixed top space in enterprise page

--- src/pages/enterprise/Content.js
+++ src/pages/enterprise/Content.js
@@ -6,25 +6,30 @@ import Form from './Form';
 import Pros from './Pros';
 
 const Content = ({ theme }) => (
-  <Container
-    pt={[
-      theme.navbarHeights.mobile,
-      theme.navbarHeights.mobile,
-      theme.navbarHeights.desktop,
-    ]}
-    px={'15px'}
-  >
-    <Flex.spaceBetween
-      flexDirection={['column', 'column', 'row']}
-      my={[2, 2, 5, 6]}
+  <Container>
+    <Box
+      pt={[
+        theme.navbarHeights.mobile,
+        theme.navbarHeights.mobile,
+        theme.navbarHeights.desktop,
+      ]}
+      px={'15px'}
     >
-      <Box width={[1, 1, 2/5]}>
-        <Pros />
-      </Box>
-      <Box width={[1, 1, 1/2]}>
-        <Form />
-      </Box>
-    </Flex.spaceBetween>
+      <Flex.spaceBetween
+        flexDirection={['column', 'column', 'row']}
+        my={[5, 5, 5, 6]}
+      >
+        <Box width={[1, 1, 2/5]}>
+          <Pros />
+        </Box>
+        <Box
+          width={[1, 1, 1/2]}
+          mt={[4, 4, 0]}
+        >
+          <Form />
+        </Box>
+      </Flex.spaceBetween>
+    </Box>
   </Container>
 )
 

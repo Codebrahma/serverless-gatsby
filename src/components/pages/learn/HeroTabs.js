2a822170ead2109eb2ebbf280ed3464c7c5c6c9e
Kameshwaran <kamesh@codebrahma.com>
2018-08-23 16:29:12 +0530

    Added hero tabs

---
+++ src/components/pages/learn/HeroTabs.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { Box, Card, DropDown } from 'serverless-design-system/src'
+import Tabs from 'src/components/Tabs'
+
+const data = [
+  { label: 'overview', value: 'overview' },
+  { label: 'use cases', value: 'use-cases' },
+  { label: 'comparisons', value: 'comparisons' },
+  { label: 'case studies', value: 'case-studies' }
+]
+
+export default () => (
+  <Box>
+    <Box display={[ 'none', 'none', 'block' ]}>
+      <Card
+        display="inline-block"
+        border={"1px solid rgba(255, 255, 255, 0.2)"}
+      >
+        <Tabs
+          data={data}
+          labelStyle={{
+            color: 'white',
+            fontFamily: 'serverless',
+            fontSize: 1,
+            lineHeight: 1,
+            letterSpacing: '0.4px',
+          }}
+          tabContainerStyle={{
+            px: 2,
+            py: '16px'
+          }}
+          selectedTabContainerStyle={{
+            background: `rgba(255, 255, 255, 0.3)`
+          }}
+          selectedValue='overview'
+          onClick={console.log}
+        />
+      </Card>
+    </Box>
+
+    <Box display={[ 'block', 'block', 'none' ]}>
+      <DropDown
+        options={data}
+        value='overview'
+        fieldContainerProps={{ background: 'transparent' }}
+        placeholderProps={{ color: 'white' }}
+      />
+    </Box>
+  </Box>
+)
--- src/components/pages/learn/overview/Hero/index.js
+++ src/components/pages/learn/overview/Hero/index.js
@@ -1,24 +1,30 @@
 import React from 'react'
 
-import { Flex } from 'serverless-design-system/src'
+import { Box, Flex } from 'serverless-design-system/src'
 import { HeroWrapper } from 'src/fragments'
-import HeroLeftSection from './LeftSection'
-import HeroRightSection from './RightSection'
-import LearnOverviewBg from '../../../../../assets/images/learnOverviewBg.png'
+import Description from './Description'
+import Actions from './Actions'
+import HeroTabs from '../../HeroTabs'
+import LearnOverviewBg from 'src/assets/images/learnOverviewBg.png'
 
 const DashboardHero = () => (
   <HeroWrapper
     backgroundImage={`url(${LearnOverviewBg})`}
     backgroundSize='contain'
   >
-    <Flex
-      flexDirection={['column', 'column', 'row']}
-      mb={[5, 5, 6, 9]}
-      px={[3, 3, 2]}
-    >
-      <HeroLeftSection />
-      <HeroRightSection />
-    </Flex>
+    <Box px={[3, 3, 2]}>
+      <Box mb={[2, 2, 5]}>
+        <HeroTabs />
+      </Box>
+
+      <Flex
+        flexDirection={['column', 'column', 'row']}
+        mb={[5, 5, 6, 9]}
+      >
+        <Description />
+        <Actions />
+      </Flex>
+    </Box>
   </HeroWrapper>
 )
 

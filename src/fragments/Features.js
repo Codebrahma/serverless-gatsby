751c115b310ce48e5f698847858d4659da277a09
Kameshwaran <kamesh@codebrahma.com>
2018-08-24 10:10:44 +0530

    Added title, description and leading number props

--- src/fragments/Features.js
+++ src/fragments/Features.js
@@ -7,15 +7,36 @@ import {
   Feature as FeatureItem,
   Heading,
   Row,
+  Text,
 } from 'serverless-design-system/src'
 
-const Features = ({ features }) => (
+const Features = ({ title, description, features, leadingNumber }) => (
   <Container>
     <Box px={2} pb={[300, 300, 10, 10]}>
       <Column px={[0, 0, 2, 5]} mt={5}>
-        <Heading.h2 align='center' fontFamily='SoleilSb' my={4}>
-          Features
-        </Heading.h2>
+        <Box my={4}>
+          <Heading.h2
+            align="center"
+            fontFamily="SoleilSb"
+            lineHeight="1.4"
+            my={1}
+          >
+            {title}
+          </Heading.h2>
+          {
+            description && (
+              <Text.p
+                align="center"
+                fontFamily="Soleil"
+                fontSize={1}
+                lineHeight={3}
+                color="gray.2"
+              >
+                { description }
+              </Text.p>
+            )
+          }
+        </Box>
         <Row flexWrap='wrap' mt={3}>
           {features.map(({ header, img, content }, index) => (
             <FeatureItem
@@ -24,6 +45,7 @@ const Features = ({ features }) => (
               content={content}
               img={img}
               rightAligned={index % 2 === 0}
+              leadingNumber={leadingNumber}
             />
           ))}
         </Row>
@@ -31,5 +53,11 @@ const Features = ({ features }) => (
     </Box>
   </Container>
 )
+
+Features.defaultProps = {
+  title: 'Features',
+  description: null,
+  leadingNumber: true,
+}
 
 export default Features

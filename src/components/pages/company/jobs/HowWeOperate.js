0406f0efca09730086cb8949bab2da0388561ad0
Kameshwaran <kamesh@codebrahma.com>
2018-08-28 01:59:36 +0530

    Fixed company pages design

--- src/components/pages/company/jobs/HowWeOperate.js
+++ src/components/pages/company/jobs/HowWeOperate.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container, Flex, Box, P, TextWithIcon, Heading, Image } from 'serverless-design-system/src'
+import { Container, Column, Flex, Box, P, TextWithIcon, Heading, Image } from 'serverless-design-system/src'
 
-import redRectangleDots from '../../../../assets/images/redRectangleDots.png'
-import team from '../../../../assets/images/team.png'
+import redRectangleDots from 'src/assets/images/redRectangleDots.png'
+import team from 'src/assets/images/team.png'
 
 const benefitList = [
   'Quality healthcare, dental, and vision plans',
@@ -13,40 +13,59 @@ const benefitList = [
   'Team Retreats',
 ]
 
-// TODO: Refactor change this to 
+// TODO: Refactor change this to
 const HorizontalLine = styled.hr`
   border: 1px solid #dfe5e6
 `
 
-// TODO: Add the non importable image in front of each benefit
 const HowWeOperate = () => (
-  <Container width={1} mt={6} px={3}>
-    <Flex.column>
+  <Container>
+    <Column>
       <TextWithIcon
         iconSrc={redRectangleDots}
-        iconHeight='32px'
-        iconWidth='50px'
-        iconTop='10px'
-        iconLeft='-10px'
+        iconHeight="32px"
+        iconWidth="52px"
+        iconTop="5px"
+        iconLeft="-10px"
+        backgroundSize="contain"
       >
-        <Heading.h1>How We Operate</Heading.h1>
+        <Heading.h1
+          fontFamily="SoleilBk"
+          lineHeight={0}
+          letterSpacing="h2"
+          mt={1}
+        >
+          How We Operate
+        </Heading.h1>
       </TextWithIcon>
       <Flex flexDirection={['column', 'column', 'row']}>
         <Box width={[1, 1, 0.5]}>
-          <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</P>
-          <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</P>
+          <P>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </P>
+          <P>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </P>
         </Box>
          <Flex justifyContent='flex-end' width={[1, 1, 0.5]}>
-          <Box width={[1, 1, 9/10, 9/10, 7/10]} boxShadow='2px 2px 8px 0 #eaeaea' border='1px solid #eaeaea' px={4} py={3}>
-            <Heading.h4 fontSize={3} fontFamily='SoleilBk'>Our Benefits</Heading.h4>
+          <Box
+            width={[1, 1, 9/10, 9/10, 7/10]}
+            boxShadow='2px 2px 8px 0 #eaeaea'
+            border='1px solid #eaeaea'
+            px={4}
+            py={3}
+          >
+            <Heading.h4 fontSize={3} fontFamily='SoleilBk'>
+              Our Benefits
+            </Heading.h4>
             <HorizontalLine />
             {benefitList.map((benefit, index) => <P key={index} mt={4} fontFamily='Soleilbk'>{benefit}</P>)}
           </Box>
         </Flex>
       </Flex>
       <Image src={team} width={1} mt={4} />
-    </Flex.column>
+    </Column>
   </Container>
 )
 
-export default HowWeOperate 
+export default HowWeOperate
--- src/components/pages/company/jobs/OpenPositions.js
+++ src/components/pages/company/jobs/OpenPositions.js
@@ -1,46 +1,65 @@
 import React from 'react'
 
-import { Box, Flex, Container, Heading, P } from 'serverless-design-system/src'
+import {
+  Box,
+  Column,
+  Flex,
+  Container,
+  Heading,
+  P,
+  ResponsiveStack
+} from 'serverless-design-system/src'
+import { ServerlessUserGroupStack } from 'src/fragments'
 
 const jobs = [
-  { type: 'Engineering', title: 'Full Stack Engineer', location: 'San Francisco', duration: 'Full-time' },
-  { type: 'Engineering', title: 'Senior Software Engineer, Open Source Tools', location: 'San Francisco', duration: 'Full-time' },
-  { type: 'Engineering', title: 'Senior/Principal Engineer Distributed Systems', location: 'San Francisco', duration: 'Full-time' },
-  { type: 'Growth', title: 'Growth and Brand Designer', location: 'San Francisco', duration: 'Full-time' },
-  { type: 'Growth', title: 'Growth Engineer', location: 'San Francisco', duration: 'Full-time' },
-  { type: 'Growth', title: 'Lead Developer Evangelist', location: 'San Francisco', duration: 'Full-time' },
+  {
+    heading: 'Engineering',
+    title: 'Full Stack Engineer',
+    location: 'San Francisco        Full-time', //NEED A FIX FOR THIS.
+  },
+  {
+    heading: 'Engineering',
+    title: 'Senior Software Engineer, Open Source Tools',
+    location: 'San Francisco       Full-time'
+  },
+  {
+    heading: 'Engineering',
+    title: 'Senior/Principal Engineer Distributed Systems',
+    location: 'San Francisco        Full-time'
+  },
+  {
+    heading: 'Growth',
+    title: 'Growth and Brand Designer',
+    location: 'San Francisco        Full-time'
+  },
+  {
+    heading: 'Growth',
+    title: 'Growth Engineer',
+    location: 'San Francisco        Full-time'
+  },
+  {
+    heading: 'Growth',
+    title: 'Lead Developer Evangelist',
+    location: 'San Francisco        Full-time'
+  },
 ]
 
-// TODO: Remove hardcoded color
-const OpenPositionCard = ({ type, title, location, duration }) => (
-  <Flex.column
-    width={[1, 4/10, 3/10]}
-    boxShadow='2px 2px 8px 0 #eaeaea'
-    p={2}
-    m={2}
-    minWidth={[7/10, '300px']}
-    alignItems='flex-start'
-  >
-    <P fontSize={0}>{type}</P>
-    <Heading.h3 fontFamily='SoleilBk' fontSize={4}>{title}</Heading.h3>
-    <Flex justifyContent='space-between' width={1}>
-      <P fontSize={0}>{location}</P>
-      <P fontSize={0}>{duration}</P>
-    </Flex>
-  </Flex.column>
-)
-
-
 const OpenPositions = () => (
-  <Container maxWidth={1} mt={8}>
-    <Flex.column>
+  <Container>
+    <Column>
       <Flex justifyContent='center'>
-        <Heading.h3 fontFamily='SoleilSb'>Open Positions</Heading.h3>
+        <Heading.h2 fontFamily='SoleilSb'>
+          Open Positions
+        </Heading.h2>
       </Flex>
-      <Flex flexDirection={['column', 'column', 'row']} flexWrap='wrap' justifyContent='center' alignItems={['center', 'left']}>
-        {jobs.map(({ type, title, location, duration }) => <OpenPositionCard key={type} type={type} title={title} location={location} duration={duration} />)}
-      </Flex>
-    </Flex.column>
+      <ResponsiveStack
+        flexWrap='wrap'
+        justifyContent='center'
+        alignItems={['center', 'left']}
+      >
+        <ServerlessUserGroupStack items={jobs} />
+      </ResponsiveStack>
+    </Column>
   </Container>
 )
 

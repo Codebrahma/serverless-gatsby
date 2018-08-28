31caf17b898e45c8c701f90d59d6a97490afc8ea
Kameshwaran <kamesh@codebrahma.com>
2018-08-28 19:29:52 +0530

    Updated team card

--- src/fragments/TeamMemberImageCard.js
+++ src/fragments/TeamMemberImageCard.js
@@ -13,7 +13,7 @@ import dotGridVertical from 'src/assets/images/dot-grid-vertical.png'
 const TeamMemberImageCard = ({ name, position, image }) => (
   <Relative
     mt={4}
-    mx={[0, 1]}
+    mx={[0, 1, '14px']}
     width="176px"
   >
     <Image

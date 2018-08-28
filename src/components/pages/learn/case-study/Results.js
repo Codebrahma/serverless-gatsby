9b14bb60e41fd69c0003ef72d025393aa82a8e9f
Kameshwaran <kamesh@codebrahma.com>
2018-08-28 22:14:22 +0530

    Fixed padding top

--- src/components/pages/learn/case-study/Results.js
+++ src/components/pages/learn/case-study/Results.js
@@ -16,7 +16,7 @@ const Results = () => (
   <AppContainer>
     <Flex.column
       mt={[4, 4, 6]}
-      mb={[4, 4, 11]}
+      mb={[4, 4, 14]}
     >
       <Flex justifyContent={['left', 'left', 'center']}>
         <TextWithIcon
@@ -37,11 +37,19 @@ const Results = () => (
           </Heading.h2>
         </TextWithIcon>
       </Flex>
-      <Box maxWidth='800px' mx='auto'>
-        <P>Feedback from the client was uniformly positive. They exceeded their goal with over a million individual users and the site was visited from over 50 countries - it was even mentioned in a tweet from David Guetta himself! They also met their goals from a performance point of view. In fact, the performance turned out to follow the opposite pattern of what’s expected in traditional architectures - when they received more traffic, the site actually got faster.</P>
-        <P>In the end the Serverless Framework, and a serverless approach in general, helped Parallax deliver an experience that would have been much much more difficult and much more expensive using a traditional application architecture.</P>
+      <Flex.center
+        flexDirection="column"
+        width={[1, 1, 4/6]}
+        mx="auto"
+      >
+        <P>
+          Feedback from the client was uniformly positive. They exceeded their goal with over a million individual users and the site was visited from over 50 countries - it was even mentioned in a tweet from David Guetta himself! They also met their goals from a performance point of view. In fact, the performance turned out to follow the opposite pattern of what’s expected in traditional architectures - when they received more traffic, the site actually got faster.
+        </P>
+        <P>
+          In the end the Serverless Framework, and a serverless approach in general, helped Parallax deliver an experience that would have been much much more difficult and much more expensive using a traditional application architecture.
+        </P>
         <Image src={learnCasestudyResults} alt='learn-results' mt={3}/>
-      </Box>
+      </Flex.center>
     </Flex.column>
   </AppContainer>
 )

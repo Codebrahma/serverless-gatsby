c10dba72b251ebd910d90d8105984c626306a1f0
Kameshwaran <kamesh@codebrahma.com>
2018-09-03 19:04:13 +0530

    Added blog layout

---
+++ src/layouts/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Box, Column } from 'serverless-design-system/src'
+import { Header, Footer } from 'src/components'
+
+const BlogLayout = ({ children, prefooter, transparentHeader }) => (
+  <Column width={1}>
+    <Header transparent={transparentHeader} />
+    <Box width={1}>{children}</Box>
+    <Footer prefooter={prefooter} />
+  </Column>
+)
+
+export default BlogLayout

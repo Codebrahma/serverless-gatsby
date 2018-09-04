8cd3dba6684d36c8abf0595cf6d27a1b7a063bb4
Kameshwaran <kamesh@codebrahma.com>
2018-09-04 15:56:11 +0530

    blog templates addition

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,65 @@
 import React from 'react'
-import BlogLayout from '../layouts/Blog'
+import { Text, Heading, Column, Row, Image } from 'serverless-design-system/src'
+import BlogLayout from 'src/layouts/Blog'
+import { AppContainer } from 'src/components'
+import AuthorsData from 'src/pages/blog/generated-authors.json'
 
-export default ({ data: { blog } }) => (
+export default ({ data: { blog: { id, frontmatter, content } } }) => (
   <BlogLayout>
-    <h1>{blog.frontmatter.title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+    <AppContainer>
+      <Column
+        width={[1, 1, 1, 0.6]}
+        mx="auto"
+        my={4}
+        alignItems="center"
+      >
+        <Text.p
+          fontFamily="Serverless"
+          fontSize={1}
+          opacity="0.4"
+          my={25}
+        >
+          { frontmatter.category || 'news' }
+        </Text.p>
+        <Heading.h1
+          fontFamily="SoleilBk"
+          letterSpacing="h4"
+          align="center"
+        >
+          { frontmatter.title }
+        </Heading.h1>
+        <Row
+          alignItems="center"
+          my={15}
+        >
+          <Text
+            fontFamily="Soleil"
+            fontSize={0}
+            color="gray.3"
+            lineHeight={2}
+          >
+            written by &nbsp;
+          </Text>
+          <Image
+            src="https://avatars3.githubusercontent.com/u/15029531?v=4&s=460"
+            height={33}
+            width={33}
+          />
+          <Text
+            fontFamily="Soleil"
+            fontSize={0}
+            color="gray.3"
+            lineHeight={2}
+          >
+            &nbsp; Adnan Rahic
+          </Text>
+        </Row>
+      </Column>
+      <Column
+        width={1}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </AppContainer>
   </BlogLayout>
 );
 
@@ -15,8 +70,7 @@ export const query = graphql`
       frontmatter {
         title
         description
-        date
-        layout
+        authors
         thumbnail
         gitLink
         category

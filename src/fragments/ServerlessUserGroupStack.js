30aa3269a78d2d6d769e76685a36fd8e2d68259a
Kameshwaran <kamesh@codebrahma.com>
2018-09-09 17:10:44 +0530

    Added serverless group link

--- src/fragments/ServerlessUserGroupStack.js
+++ src/fragments/ServerlessUserGroupStack.js
@@ -24,55 +24,69 @@ const FlexedMasonry = styled(Masonry)`
   justify-content: center;
 `
 
+const GroupCard = ({ heading, title, location, link }) => (
+  <Column
+    width="90%"
+    mx='auto'
+    my={[1, 1, 2]}
+    minHeight='128px'
+    justifyContent='center'
+  >
+    <Background background="white">
+      <Card
+        boxShadow='2px 2px 8px 0 #eaeaea'
+        border='1px solid #eaeaea'
+        p={[2, 2, 3]}
+      >
+        {
+          heading ? (
+            <Heading.h6
+              fontFamily="Soleil"
+              fontSize={0}
+              color="gray.3"
+            >
+              { heading }
+            </Heading.h6>
+          ) : null
+        }
+        <Heading.h5
+          fontFamily="SoleilBk"
+          fontSize={[2, 2, 4]}
+          m={0}
+          color="black"
+        >
+          {title}
+        </Heading.h5>
+        <P
+          fontSize={[0, 0, 2]}
+          mt={1}
+          mb={0}
+        >
+          {location}
+        </P>
+      </Card>
+    </Background>
+  </Column>
+)
+
 const ServerlessUserGroupStack = ({ items }) => (
   <FlexedMasonry
     breakpointCols={breakpointCols}
   >
     {
-      items.map(({ heading, title, location }, index) => (
-        <Column
-          key={`${title}-${index}`}
-          width={['90%', '90%', '90%']}
-          mx='auto'
-          my={[1, 1, 2]}
-          minHeight='128px'
-          justifyContent='center'
-        >
-          <Background background="white">
-            <Card
-              boxShadow='2px 2px 8px 0 #eaeaea'
-              border='1px solid #eaeaea'
-              p={[2, 2, 3]}
-            >
-              {
-                heading ? (
-                  <Heading.h6
-                    fontFamily="Soleil"
-                    fontSize={0}
-                    color="gray.3"
-                  >
-                    { heading }
-                  </Heading.h6>
-                ) : null
-              }
-              <Heading.h5
-                fontFamily="SoleilBk"
-                fontSize={[2, 2, 4]}
-                m={0}
-                color="black"
-              >
-                {title}
-              </Heading.h5>
-              <P
-                fontSize={[0, 0, 2]}
-                mt={1}
-                mb={0}
-              >
-                {location}
-              </P>
-            </Card>
-          </Background>
-        </Column>
+      items.map(({ heading, title, location, link }, index) => (
+        link ? (
+          <a key={`${title}-${index}`} href={link} target="_blank">
+            <GroupCard heading={heading} title={title} location={location} />
+          </a>
+        ) : (
+          <GroupCard
+            key={`${title}-${index}`}
+            heading={heading}
+            title={title}
+            location={location}
+          />
+        )
       ))
     }
   </FlexedMasonry>

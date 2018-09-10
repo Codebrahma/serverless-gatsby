08f3b57546b02811b7532cfcc12c1788523d1479
Kameshwaran <kamesh@codebrahma.com>
2018-09-10 15:55:49 +0530

    Fixed champion card alignment

--- src/components/pages/company/champions/OurChampions/index.js
+++ src/components/pages/company/champions/OurChampions/index.js
@@ -23,31 +23,33 @@ const champions = [
   {
     name: 'Takahiro Horike',
     image: Champion1,
-    position: 'Software Engineer DigitalCube Co.Ltd'
+    details: ['Software Engineer', 'DigitalCube Co.Ltd']
   },
   {
     name: 'Marcia Villalba',
     image: Champion2,
-    position: 'Senior Full-stack Developer Rovio'
+    details: ['Senior Full-stack Developer', 'Rovio']
   },
   {
     name: 'Ryan Scott Brown',
     image: Champion3,
-    position: 'Senior Software Engineer Ansible by Red Hat'
+    details: ['Senior Software Engineer', 'Ansible by Red Hat']
   },
 ]
 
-const ChampionCard = ({ name, image, position }) => (
+const ChampionCard = ({ name, image, details }) => (
   <Relative
-    width={[1, 1, 3/10]}
-    px={2}
+    width={[1, 1, 0.33]}
+    maxWidth={400}
+    minHeight={390}
+    px={[0, 0, 2]}
     py={[2, 2, 2, 0]}
   >
     <Image
       src={image}
       alt={name}
       width={1}
-      maxHeight="384px"
+      maxHeight={384}
     />
     <Absolute
       bottom="15px"
@@ -61,11 +63,19 @@ const ChampionCard = ({ name, image, position }) => (
         backgroundSize="cover"
       />
     </Absolute>
-    <Flex>
-      <Box width={4/10}></Box>
-      <Box width={6/10}>
-        <Heading.h4 mt={2} fontFamily='SoleilBk'>{name}</Heading.h4>
-        <P>{position}</P>
+    <Flex justifyContent="flex-end">
+      <Box width={[0.6, 0.6, 0.9, 0.65]}>
+        <Heading.h4
+          mt={2}
+          fontFamily='SoleilBk'
+          fontSize={[3, 3, 3, 4]}
+        >
+          {name}
+        </Heading.h4>
+
+        <P fontSize={[1, 1, 2]}>
+          { details.map((detail) => (<Box>{ detail }</Box>)) }
+        </P>
       </Box>
     </Flex>
   </Relative>
@@ -73,7 +83,7 @@ const ChampionCard = ({ name, image, position }) => (
 
 const OurChampions = () => (
   <AppContainer>
-    <Box py={[5, 5, 4, 6]}>
+    <Box pt={2} pb={[8, 8, 5, 6]}>
       <Box my={[1, 2, 3, 4]}>
         <Heading.h2
           align="center"
@@ -96,17 +106,16 @@ const OurChampions = () => (
         }
       </Box>
       <ResponsiveStack
+        alignItems="center"
         justifyContent="center"
         mt={[1, 2, 2, 3]}
         my={12}
       >
         {
-          champions.map(({ name, image, position }) => (
+          champions.map((champion, index) => (
             <ChampionCard
-              key={name}
-              name={name}
-              image={image}
-              position={position}
+              key={`champion-${index}`}
+              {...champion}
             />
           ))
         }

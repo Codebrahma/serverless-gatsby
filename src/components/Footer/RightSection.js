4d3027ca436bcf1898a2c816d3c1b23bd1648754
Kameshwaran <kamesh@codebrahma.com>
2018-09-10 12:18:49 +0530

    Updated border radius

--- src/components/Footer/RightSection.js
+++ src/components/Footer/RightSection.js
@@ -46,13 +46,16 @@ const Subscribe = () => (
         placeholder='email address'
         px={[1, 1, 1]}
         py={2}
+        m={0}
         width={[1, 1, 0.6]}
+        borderRadius={0}
         boxSizing='border-box'
       />
       <Button
         width={['35%', '35%', '40%']}
         px={[0, 0, 1]}
         py={2}
+        m={0}
         textAlign='center'
         border={0}
         fontSize={2}

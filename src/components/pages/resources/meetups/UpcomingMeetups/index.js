1cd5e4f059e1141b089fbe05904831930bb41425
Kameshwaran <kamesh@codebrahma.com>
2018-09-12 12:48:51 +0530

    Removed unnecessary imports

--- src/components/pages/resources/meetups/UpcomingMeetups/index.js
+++ src/components/pages/resources/meetups/UpcomingMeetups/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import {
   Column,
   ResponsiveStack,
-  Heading,
-  TextWithIcon
 } from 'serverless-design-system/src'
 import { AppContainer } from 'src/components'
 import { MeetupCard, TitleWithIcon } from 'src/fragments'

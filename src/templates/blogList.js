93a571bfcaf198bee5f813ac4685616a583e0e89
Kameshwaran <kamesh@codebrahma.com>
2018-09-12 18:11:18 +0530

    Removed class def

--- src/templates/blogList.js
+++ src/templates/blogList.js
@@ -6,30 +6,26 @@ import BlogListPrefooter from 'src/components/pages/blog/ListPrefooter'
 import { Divider } from 'src/fragments'
 import { Pagination } from 'src/components'
 
-export default class Blogs extends React.Component {
-  render() {
-    const {
-      data: { blogs: { edges, totalCount }, highlighted },
-      pathContext: { start, limit }
-    } = this.props
+export default ({
+  data: { blogs: { edges, totalCount }, highlighted },
+  pathContext: { start, limit }
+}) => {
+  const totalPages = Math.ceil(totalCount / limit)
+  const currentPage = start/limit
 
-    const totalPages = Math.ceil(totalCount / limit)
-    const currentPage = start/limit
-
-    return (
-      <BlogLayout prefooter={BlogListPrefooter}>
-        { currentPage === 0 && (
-            <React.Fragment>
-              <HighlightedBlogs blogs={highlighted.edges.map(({ node }) => node)} />
-              <Divider visibleInSmallScreens />
-            </React.Fragment>
-          )
-        }
-        <BlogPreview blogs={edges.map(({ node }) => node)} />
-        <Pagination total={totalPages} current={currentPage} />
-      </BlogLayout>
-    )
-  }
+  return (
+    <BlogLayout prefooter={BlogListPrefooter}>
+      { currentPage === 0 && (
+          <React.Fragment>
+            <HighlightedBlogs blogs={highlighted.edges.map(({ node }) => node)} />
+            <Divider visibleInSmallScreens />
+          </React.Fragment>
+        )
+      }
+      <BlogPreview blogs={edges.map(({ node }) => node)} />
+      <Pagination total={totalPages} current={currentPage} />
+    </BlogLayout>
+  )
 }
 
 export const query = graphql`

7223125783cfce67f32c3b58f5f1e7eb86baec02
Kameshwaran <kamesh@codebrahma.com>
2018-09-14 21:51:24 +0530

    Added overlay

--- src/components/pages/blog/HighlightedBlogs/Item.js
+++ src/components/pages/blog/HighlightedBlogs/Item.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+  Absolute,
   Box,
   Background,
   Flex,
@@ -16,7 +17,7 @@ import {
 } from 'src/utils/blog'
 import Categories from '../Categories'
 
-const Item = ({ id, frontmatter, numSlides, goToSlide, currentSlide }) => {
+const Item = ({ id, frontmatter }) => {
   const author = getAuthorInfo({ frontmatter })
   const { title, category: categoryIds, description, heroImage } = frontmatter
 
@@ -28,67 +29,76 @@ const Item = ({ id, frontmatter, numSlides, goToSlide, currentSlide }) => {
         backgroundSize="cover"
         backgroundPosition="center"
       >
-        <AppContainer>
-          <Flex.verticallyCenter
-            flexDirection="column"
-            width={[1, 1, 1, 0.65]}
-            m="auto"
-            pt={4}
-          >
-            <Categories categoryIds={categoryIds} />
-            <BlockLink to={getBlogLink(id)}>
-              <Heading.h2
-                fontFamily="SoleilBk"
-                fontSize={[4, 4, 5, 7]}
-                align="center"
-                lineHeight={[3, 3, 2, 1]}
-                color="white"
-              >
-                { title }
-              </Heading.h2>
-            </BlockLink>
-            <Box
-              width={[1, 1, 1, 0.9]}
-              px={[0, 0, 0, 4]}
+        <Absolute
+          height="fullHeight"
+          width={1}
+          bg="black"
+          zIndex={0}
+          style={{ opacity: 0.35 }}
+        />
+        <Absolute>
+          <AppContainer>
+            <Flex.verticallyCenter
+              flexDirection="column"
+              width={[1, 1, 1, 0.65]}
+              m="auto"
+              pt={4}
             >
-              <Text.p
-                fontFamily="SoleilBk"
-                color="white"
-                lineHeight={1.63}
-                mt={[0, 0, 1, 15]}
-                align="center"
+              <Categories categoryIds={categoryIds} />
+              <BlockLink to={getBlogLink(id)}>
+                <Heading.h2
+                  fontFamily="SoleilBk"
+                  fontSize={[4, 4, 5, 7]}
+                  align="center"
+                  lineHeight={[3, 3, 2, 1]}
+                  color="white"
+                >
+                  { title }
+                </Heading.h2>
+              </BlockLink>
+              <Box
+                width={[1, 1, 1, 0.9]}
+                px={[0, 0, 0, 4]}
               >
-                { description }
-              </Text.p>
-            </Box>
+                <Text.p
+                  fontFamily="SoleilBk"
+                  color="white"
+                  lineHeight={1.63}
+                  mt={[0, 0, 1, 15]}
+                  align="center"
+                >
+                  { description }
+                </Text.p>
+              </Box>
 
-            <BlockLink to={getAuthorLink(author.id)}>
-              <Flex.center>
-                <Text
-                  fontFamily="Soleil"
-                  fontSize={0}
-                  color="white"
-                  lineHeight={2}
-                >
-                  written by &nbsp;
-                </Text>
-                <Image
-                  src={author.avatar}
-                  height={33}
-                  width={33}
-                />
-                <Text
-                  fontFamily="Soleil"
-                  fontSize={0}
-                  color="white"
-                  lineHeight={2}
-                >
-                  &nbsp; {author.name}
-                </Text>
-              </Flex.center>
-            </BlockLink>
-          </Flex.verticallyCenter>
-        </AppContainer>
+              <BlockLink to={getAuthorLink(author.id)}>
+                <Flex.center>
+                  <Text
+                    fontFamily="Soleil"
+                    fontSize={0}
+                    color="white"
+                    lineHeight={2}
+                  >
+                    written by &nbsp;
+                  </Text>
+                  <Image
+                    src={author.avatar}
+                    height={33}
+                    width={33}
+                  />
+                  <Text
+                    fontFamily="Soleil"
+                    fontSize={0}
+                    color="white"
+                    lineHeight={2}
+                  >
+                    &nbsp; {author.name}
+                  </Text>
+                </Flex.center>
+              </BlockLink>
+            </Flex.verticallyCenter>
+          </AppContainer>
+        </Absolute>
       </Background>
   );
 }

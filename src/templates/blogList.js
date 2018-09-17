898f14ab697e55098f8dee8bc0f5f4735b52a72c
Kameshwaran <kamesh@codebrahma.com>
2018-09-17 21:29:08 +0530

    Updated highlighted blogs listing

--- src/templates/blogList.js
+++ src/templates/blogList.js
@@ -5,6 +5,7 @@ import BlogPreview from 'src/components/pages/blog/Preview'
 import BlogListPrefooter from 'src/components/pages/blog/ListPrefooter'
 import { Helmet } from 'src/fragments'
 import { Pagination } from 'src/components'
+import HighlightedBlogList from 'src/constants/highlighted-blogs.json'
 
 export default ({
   data: { blogs: { edges, totalCount }, highlighted },
@@ -12,6 +13,15 @@ export default ({
 }) => {
   const totalPages = Math.ceil(totalCount / limit)
   const currentPage = start/limit
+  const highlightedBlogs = highlighted
+    .edges
+    .map(({ node }) => node)
+    .sort((blog1, blog2) => {
+      const blog1Priority = HighlightedBlogList.indexOf(blog1.id)
+      const blog2Priority = HighlightedBlogList.indexOf(blog2.id)
+      if (blog1Priority === blog2Priority) { return 0 }
+      return (( blog1Priority > blog2Priority ) ? 1 : -1)
+    })
 
   return (
     <BlogLayout prefooter={BlogListPrefooter}>
@@ -22,7 +32,7 @@ export default ({
       {
         currentPage === 0 && (
           <React.Fragment>
-            <HighlightedBlogs blogs={highlighted.edges.map(({ node }) => node)} />
+            <HighlightedBlogs blogs={highlightedBlogs} />
           </React.Fragment>
         )
       }

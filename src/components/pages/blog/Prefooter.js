3b313fae4e2265cd9d93a7795926bf02da203617
Kameshwaran <kamesh@codebrahma.com>
2018-09-18 16:19:01 +0530

    Added newsletter form handling

--- src/components/pages/blog/Prefooter.js
+++ src/components/pages/blog/Prefooter.js
@@ -10,7 +10,30 @@ import {
   TertiaryButton,
   Card,
 } from 'serverless-design-system/src'
+import { NewsLetterForm } from 'src/components'
 import blogFooterBackground from 'src/assets/images/blog-footer-background.png'
+
+const SubmitButton = (props) => (
+  <TertiaryButton
+    width={0.3}
+    height={60}
+    fontSize={1}
+    {...props}
+  >
+    sign up
+  </TertiaryButton>
+)
+
+const Title = ({ component: HeadingComp, color }) => (
+  <HeadingComp
+    color={color}
+    fontFamily="SoleilLt"
+    letterSpacing="0"
+    lineHeight={2}
+  >
+    Join 12,000+ other serverless devs &amp; keep up to speed on the latest serverless trends.
+  </HeadingComp>
+)
 
 export default () => (
   <React.Fragment>
@@ -29,40 +52,24 @@ export default () => (
             p={[1, 1, 5, 7]}
             pr={2}
           >
-            <Heading.h4
-              color="white"
-              fontFamily="SoleilLt"
-              letterSpacing="0"
-              lineHeight={2}
-            >
-              Join 12,000+ other serverless devs &amp; keep up to speed on the latest serverless trends.
-            </Heading.h4>
+            <Title component={Heading.h4} color="white" />
           </Flex>
           <Flex
             justifyContent="flex-end"
+            alignItems="center"
             width={[1, 1, 0.5]}
-            p={[1, 1, 5, 7]}
+            p={[1, 1, 4, 4, 7]}
           >
-            <TextField
-              width={0.6}
-              height={60}
-              bg="#ffffff33"
-              border="none"
-              px={2}
-              fontSize={1}
-              lineHeight={4}
-              placeholder="enter email address"
-              placeholderColor="white"
-              fontFamily="Serverless"
-              letterSpacing="text"
+            <NewsLetterForm
+              emailFieldProps={{ width: 0.6,
+                height: 60,
+                bg: "#ffffff33",
+                fontSize: 1,
+                placeholder: "enter email address"
+              }}
+              btnComponent={SubmitButton}
+              wrapperProps={{ width: 1, color: 'white' }}
             />
-            <TertiaryButton
-              width={0.3}
-              height={60}
-              fontSize={1}
-            >
-              sign up
-            </TertiaryButton>
           </Flex>
         </ResponsiveStack>
       </Background>
@@ -85,40 +92,18 @@ export default () => (
               px={2}
               pb={2}
             >
-              <Heading.h5
-                fontFamily="SoleilLt"
-                letterSpacing="0"
-                lineHeight={2}
-              >
-                Join 12,000+ other serverless devs &amp; keep up to speed on the latest serverless trends.
-              </Heading.h5>
+              <Title component={Heading.h5} />
             </Flex>
-            <Flex.column
-              alignItems="center"
-              width={1}
-              px={2}
-            >
-              <TextField
-                width={1}
-                height={60}
-                bg="#8c8c8c33"
-                border="none"
-                px={2}
-                fontSize={1}
-                lineHeight={4}
-                placeholder="enter email address"
-                placeholderColor="white"
-                fontFamily="Serverless"
-                letterSpacing="text"
-              />
-              <Button
-                width={0.5}
-                height={60}
-                fontSize={1}
-              >
-                sign up
-              </Button>
-            </Flex.column>
+            <NewsLetterForm
+              wrapper={Flex.column}
+              wrapperProps={{ alignItems: 'center', width: 1, px: 2 }}
+              emailFieldProps={{
+                bg: "#8c8c8c33",
+                placeholder: "enter email address",
+                placeholderColor: "gray.3",
+                color: "gray.3"
+              }}
+            />
           </ResponsiveStack>
         </Background>
       </Card>
---
+++ src/utils/storage.js
@@ -0,0 +1,157 @@
+/**
+  LocalForage with cookie fallbacks
+*/
+import localforage from 'localforage'
+import { createCookie, readCookie, eraseCookie } from './cookie'
+const isServer = typeof window === 'undefined'
+const localStorageSupport = localStorageSupported()
+const localForageSupport = storageSupported()
+
+localforage.config({
+  name: 'site',
+})
+
+export function setItem(key, value, callback) {
+  if (isServer) return false
+  if (localForageSupport) {
+    // if callback return with callback else return Promise
+    return (callback) ? localforage.setItem(key, value, callback) : localforage.setItem(key, value)
+  }
+  // no localForage support. Fallback to cookie
+  try {
+    createCookie(key, value)
+    // default return promise
+    return (callback) ? callback(value) : new Promise((resolve, reject) => resolve(value))
+  } catch (e) {
+    // Cookies failed set window.var
+    window[key] = value
+    return (callback) ? callback(value) : new Promise((resolve, reject) => resolve(value))
+  }
+}
+
+export function getItem(key, callback) {
+  if (isServer) return false
+  if (localForageSupport) {
+    // if callback return with cb or do promise
+    return (callback) ? localforage.getItem(key, callback) : localforage.getItem(key)
+  }
+  // no localStorage support. Fallback to cookie
+  try {
+    const value = parse(readCookie(key))
+    // if callback return with callback else return Promise
+    return (callback) ? callback(value) : new Promise((resolve, reject) => resolve(value))
+  } catch (e) {
+    // Cookies failed use window.var
+    const value = window[key]
+    return (callback) ? callback(value) : new Promise((resolve, reject) => resolve(value))
+  }
+}
+
+export function removeItem(key, callback) {
+  if (isServer) return false
+  if (localForageSupport) {
+    // if callback return with callback else return Promise
+    return (callback) ? localforage.removeItem(key, callback) : localforage.removeItem(key)
+  }
+  // no localStorage support. Fallback to cookie
+  try {
+    eraseCookie(key)
+    // if callback return with callback else return Promise
+    return (callback) ? callback(true) : new Promise((resolve, reject) => resolve(true))
+  } catch (e) {
+    // Cookies failed nullify window.var
+    window[key] = null
+    // if callback return with callback else return Promise
+    return (callback) ? callback(true) : new Promise((resolve, reject) => resolve(true))
+  }
+}
+
+export function setItemSync(key, value) {
+  if (isServer) return false
+  if (localStorageSupport) {
+    const item = JSON.stringify(value)
+    window.localStorage.setItem(key, item)
+    return parse(item)
+  }
+  // no localStorage support. Fallback to cookie
+  try {
+    createCookie(key, value)
+  } catch (e) {
+    // no cookie support. Fallback to window
+    window[key] = value
+  }
+  return value
+}
+
+export function getItemSync(key) {
+  if (isServer) return false
+  if (localStorageSupport) {
+    return parse(window.localStorage.getItem(key))
+  }
+  // no localStorage support. Fallback to cookie
+  try {
+    return parse(readCookie(key))
+  } catch (e) {
+    // no cookie support. Fallback to window
+    return (window[key]) ? (window[key]) : null
+  }
+}
+
+export function removeItemSync(key) {
+  if (isServer) return false
+  if (localStorageSupport) {
+    return localStorage.removeItem(key)
+  }
+  // no localStorage support. Fallback to cookie
+  try {
+    eraseCookie(key)
+  } catch (e) {
+    // no cookie support. Fallback to window
+    window[key] = null
+  }
+  return null
+}
+
+function storageSupported() {
+  const { INDEXEDDB, WEBSQL, LOCALSTORAGE, supports } = localforage
+  return supports(INDEXEDDB) || supports(WEBSQL) || supports(LOCALSTORAGE)
+}
+
+function localStorageSupported() {
+  if (isServer) return false
+  if ('localStorage' in window) {
+    try {
+      if (typeof localStorage === 'undefined' || typeof JSON === 'undefined') return false
+      // test for safari private
+      localStorage.setItem('CHECK_PRIVATE_SAFARI', '1')
+      localStorage.removeItem('CHECK_PRIVATE_SAFARI')
+    } catch (err) {
+      return false
+    }
+    return true
+  }
+}
+
+function parse(result) {
+  let value
+  try {
+    value = JSON.parse(result)
+    if (typeof value === 'undefined') {
+      value = result
+    }
+    if (value === 'true') {
+      value = true
+    }
+    if (value === 'false') {
+      value = false
+    }
+    if (parseFloat(value) === value && typeof value !== 'object') {
+      value = parseFloat(value)
+    }
+  } catch (e) {
+    value = result
+  }
+  return value
+}
+
+export { createCookie, readCookie, eraseCookie } from './cookie'
